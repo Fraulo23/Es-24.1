1972eaef7a6da4b918b987da9539fdc80d6a7f2d
Fraulo23 <fraulinifederico@gmail.com>
2023-11-27 16:13:13 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_24._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserire la misura del lato");
+            Quadrato l = new Quadrato();
+            l.Lato=float.Parse(Console.ReadLine());
+            Console.WriteLine("L'area del quadrato è {0}",l.Lato*l.Lato);
+            Console.ReadLine();
+        }
+    }
+}
